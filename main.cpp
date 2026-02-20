eaaefad5b0fa2c9cf809d0d02e22c7ceac9d65ff
Toky <tokyrandriamamonjy8@gmail.com>
2026-02-20 10:20:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <string>
 
-void salut(std::string &a){
+void salut(std::string &a, std::string &c){
 
     std::cout << "salut " << a << std::endl;
+    std::cout << "salut " << c << std::endl;
 
 }
 
@@ -13,6 +14,10 @@ int main(){
     std::cout << "Entrez votre nom : ";
     std::cin >> b;
     salut(b);
+    std::string d;
+    std::cout << "Entrez votre prenom : ";
+    std::cin >> d;
+    salut(d);
     
 
 
